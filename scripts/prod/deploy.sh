99384e5df02f7cfe9e3e83d2a827a8a8b9520a5d
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-10-11 20:14:46 +0200

    Fix template path

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The command deploy the infrstructure in "eu-central-1" using the S3 Bucket "readapp-backend-cloudformation"
-sam deploy --stack-name readapp-backend --s3-bucket readapp-backend-cloudformation --template ../../templates/prod.yaml --capabilities CAPABILITY_IAM --region eu-central-1
+sam deploy --stack-name readapp-backend --s3-bucket readapp-backend-cloudformation --template ./templates/prod.yaml --capabilities CAPABILITY_IAM --region eu-central-1
