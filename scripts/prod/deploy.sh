e100d64851160c7f4dd94a1c5dc5e55d55765723
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-10-26 01:16:27 +0200

    Change deplay script according to new prod SAM template

--- scripts/prod/deploy.sh
+++ scripts/prod/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# The command deploy the infrstructure in "eu-central-1" using the S3 Bucket "readapp-backend-cloudformation"
-sam deploy --stack-name readapp-backend --no-confirm-changeset --no-fail-on-empty-changeset --s3-bucket readapp-cloudformation-backend --template ./templates/prod.json --capabilities CAPABILITY_IAM --region eu-central-1
+# The command deploy the infrstructure in "eu-central-1" using the S3 Bucket "readapp-cloudformation-backend"
+rm -rf .aws-sam
+sam build --template templates/prod.yml
+sam deploy --stack-name readapp-backend --s3-bucket readapp-cloudformation-backend --capabilities CAPABILITY_IAM --region eu-central-1
